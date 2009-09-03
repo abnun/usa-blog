8e121d4511a191987738945f0c0e6f24e1d8c2c7
Markus <abnun@gmx.de>
2009-09-04 00:07:29 +0200

    Route entfernt

--- grails-app/controllers/de/webmpuls/blog/InhaltController.groovy
+++ grails-app/controllers/de/webmpuls/blog/InhaltController.groovy
@@ -3,7 +3,7 @@ package de.webmpuls.blog
 class InhaltController
 {
 	static navigation = [
-		[group: 'menutop', order: 40, title: 'Route', action: 'route'],
+		//[group: 'menutop', order: 40, title: 'Route', action: 'route'],
 		[group: 'menutop', order: 50, title: 'Impressum', action: 'impressum']
 	]
 
