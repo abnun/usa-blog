459d3684760607be18e3373e36c4ba814d068ac3
Markus <abnun@gmx.de>
2009-10-04 23:23:32 +0200

    JsecUser und Sex auseinandergezogen

---
+++ grails-app/domain/de/webmpuls/blog/Sex.groovy
@@ -0,0 +1,13 @@
+package de.webmpuls.blog
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: markus
+ * Date: 04.10.2009
+ * Time: 23:18:43
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Sex
+{
+	MALE, FEMALE
+}
